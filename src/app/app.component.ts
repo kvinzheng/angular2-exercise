50661161f832f0159f16bd95c07895b762fe4146
Kevin(Jiahong) Zheng <kvinzheng@gmail.com>
2017-04-14 17:40:27 -0700

    finished deployed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,12 +7,15 @@ import { Article } from './article/article.model';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  // addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean{
-  //   console.log(`Adding article title: ${title.value} and link: ${link.value}`);
-  //   return false;
-  // }
-
+  addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean{
+    console.log(`Adding article title: ${title.value} and link: ${link.value}`);
+    this.articles.push(new Article(title.value, link.value, 0));
+    title.value = '';
+    link.value = '';
+    return false;
+  }
   articles: Article[];
+  foo: Article;
 
   constructor() {
     this.articles = [
@@ -20,5 +23,10 @@ export class AppComponent {
       new Article('Fullstack', 'http://fullstack.io', 2),
       new Article('Angular Homepage', 'http://angular.io', 1),
     ];
+
+    this.foo = new Article('Angular2', 'http://angular.io', 3)
+  }
+  sortedArticles(): Article[]{
+    return this.articles.sort((a: Article, b: Article) => b.votes - a.votes);
   }
 }
